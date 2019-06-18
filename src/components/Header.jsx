745adf9a63191cb3e1c06d87dc7387174c07b0cb
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2019-06-18 17:42:19 -0300

    Commit #2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 
 // import { Container } from './styles';
 
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <div>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="/">Exercícios ES6</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link>Aulas</Nav.Link>
+      <NavDropdown title="Exercícios" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/m1">M1 - Módulo 1</NavDropdown.Item>
+        <NavDropdown.Item href="/m2">M2 - Módulo 2</NavDropdown.Item>
+        <NavDropdown.Item href="/m3">M3 - Módulo 3</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+</div>
   );
 }
--- src/pages/M1.jsx
+++ src/pages/M1.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class M1 extends Component {
   render() {
     return (
         <div>
-            <h1>M1</h1>
+            <Container>
+              <Row>
+                <Col md={{offset: 4}} style={{marginTop: '2rem'}}>
+                  <h1> M1 - Módulo 1 </h1> 
+                </Col> 
+              </Row>
+            </Container>
         </div>
     )
   }
