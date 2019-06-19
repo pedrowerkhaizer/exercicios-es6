05e85c72a095e87668440855ec55506c3f1c92b3
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2019-06-19 12:33:17 -0300

    Revert "added the code for public view"
    This reverts commit c33d9f70a286a1bc336462d297d06ef6dd17339a.

--- src/pages/Resex1_1.jsx
+++ src/pages/Resex1_1.jsx
@@ -4,11 +4,9 @@ import React, { Component } from 'react';
 export default class Resex1_1 extends Component {
   render() {
     return (
-      <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12" >
-        <h3 style={{ marginTop: '64px', marginBottom: '0px', fontWeight: 'bold', width: '100%' }}
-          align='center'>Então, resolvi desse jeito ai msm. Chato foi colocar esse código p exibir kkk</h3>
-        <div style={{ width: '100%', background: '#272635', borderRadius: '8px', padding: '8px', margin: '512px', marginTop: '16px' }}>
-          <code style={{ color: '#fff', textAlign: 'justify' }}>
+      <div>
+        <div style={{ background: '#272635', borderRadius: '8px', padding: '8px', margin: '512px', marginTop: '64px'}}>
+          <code style={{ color: '#fff' }}>
             class Usuario{'{'} <br />
             constructor(email, senha){'{'} <br />
             this.email = email <br />
